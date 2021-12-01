e309d4c4a5a4e5f7c7d9fa71ff44599d37a23ca4
PeterVenhuizen <peter_venhuizen@hotmail.com>
2021-12-01 09:27:08 +0100

    Done with part 2

--- SonarSweep/SonarSweep.java
+++ SonarSweep/SonarSweep.java
@@ -24,12 +24,39 @@ class SonarSweep {
             .count();      
     }
 
+    public static int sumOfList(List<Integer> l) {
+        return l.stream().reduce(0, Integer::sum);
+    }
+
+    public static int countSlidingWindowDepthIncreases(List<Integer> measurements) {
+
+        int index = 0, depthIncreases = 0, windowSize = 3;
+        while (index < measurements.size() - windowSize) {
+            try {
+                List<Integer> firstWindow = measurements.subList(index, index + 3);
+                ++index;
+                List<Integer> secondWindow = measurements.subList(index, index + 3);
+
+                if (sumOfList(secondWindow) > sumOfList(firstWindow)) 
+                    depthIncreases++;
+            } catch (IndexOutOfBoundsException e) {}
+        }
+        return depthIncreases;
+    }
+
     public static void main(String[] args) {
+
+        // part 1 check
         System.out.println(countDepthIncreases(Arrays.asList(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)) == 7);
 
+        // part 2 check
+        System.out.println(countSlidingWindowDepthIncreases(Arrays.asList(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)));
+
         try {
+            // part 1 - solution
             List<Integer> depthMeasurements = readInputFileIntoIntegerList("day_1_input.txt");
-            System.out.println(countDepthIncreases(depthMeasurements));
+            System.out.println("Part 1 answer: " + countDepthIncreases(depthMeasurements));
+            System.out.println("Part 2 answer: " + countSlidingWindowDepthIncreases(depthMeasurements));
         } catch (FileNotFoundException e) {
             System.err.println(e);
         }
