eb471c513bd0ae565c1ac20760c0f78361977f65
PeterVenhuizen <peter_venhuizen@hotmail.com>
2021-12-04 14:06:22 +0100

    Cleanup of obsolete code

--- GiantSquid/GiantSquid.java
+++ GiantSquid/GiantSquid.java
@@ -2,45 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class GiantSquid {
-    // public static List<String> DRAWS;
-    // public static List<Bingo> BOARDS = new ArrayList<>();
 
     public static BingoSubsystem readyBingoSubsystem(String inputFile) 
         throws FileNotFoundException {
             Scanner sc = new Scanner(new File(inputFile));
 
-            // first line are the draws
+            // first line contains the draws
             List<String> draws = Arrays.asList(sc.nextLine().split(","));
-            // GiantSquid.DRAWS = Arrays.asList(sc.nextLine().split(","));
 
-            // after that come the boards
+            // after that come the boards, consisting of five numbers per 
+            // line and five lines per board
             List<Bingo> bingoBoards = new ArrayList<>();
             List<String> newBoard = new ArrayList<>();
             while (sc.hasNext()) {
 
-                // System.out.println(board.size());
                 String line = sc.nextLine();
-                if (line.isEmpty() && newBoard.size() == 25) {
+                boolean isBoardComplete = line.isEmpty() && newBoard.size() == 25;
+                if (isBoardComplete) {
                     bingoBoards.add(new Bingo(newBoard));
-
-                    // GiantSquid.BOARDS.add(new Bingo(board));
                     newBoard = new ArrayList<>();
                 }
 
                 else if (!line.isEmpty()) {
-                    // System.out.println(line.trim().replaceAll("\\s+", " "));
                     String[] lineArray = line.trim().replaceAll("\\s+", " ").split(" ");
                     for (int i = 0; i < lineArray.length; i++) {
                         newBoard.add(lineArray[i]);
                     }
                 }
             }
-
-            // GiantSquid.BOARDS.add(new Bingo(board));
-            bingoBoards.add(new Bingo(newBoard));
-
             sc.close();
 
+            bingoBoards.add(new Bingo(newBoard));
             return new BingoSubsystem(draws, bingoBoards);
         }
 
@@ -62,73 +54,6 @@ public class GiantSquid {
             System.out.println("Part 2 test (should be 1924): " + testSystem.getLastWinner().getFinalScore());
             System.out.println("Part 2 answer: " + bingoSystem.getLastWinner().getFinalScore());
 
-
-            // readInput("test_input.txt");
-
-            // List<Bingo> winners = new ArrayList<>();
-
-            // List<String> draws = GiantSquid.DRAWS;
-            // Bingo winningBoard;
-            // drawloop:
-            // for (int i = 0; i < draws.size(); i++) {
-
-
-            //     String nextDraw = draws.get(i);
-            //     // System.out.println(nextDraw);
-            //     System.out.println("Last number: " + nextDraw);
-
-            //     for (int j = 0; j < GiantSquid.BOARDS.size(); j++) {
-            //         Bingo board = GiantSquid.BOARDS.get(j);
-
-            //         if (!board.hasWon()) {
-
-            //             board.markNumber(nextDraw);
-
-            //             if (board.hasWon()) {
-            //                 winners.add(board);
-
-            //                 if (winners.size() == 1) {
-            //                     System.out.println("First winner: " + board.getFinalScore(Integer.parseInt(nextDraw)));
-            //                 }
-
-            //                 System.out.println("Number of winners: " + winners.size());
-            //                 // board.printBoard();
-            //                 // board.printMarks();
-                            
-            //                 if (winners.size() == GiantSquid.BOARDS.size()) {
-            //                     System.out.println("Last winner: " + board.getFinalScore(Integer.parseInt(nextDraw)));
-            //                     break drawloop;
-            //                 }
-            //             }
-            //         }
-
-            //     }
-
-                // GiantSquid.BOARDS.forEach(board -> {
-                //     board.markNumber(nextDraw);
-
-
-
-                // });
-
-                // winningBoard = GiantSquid.BOARDS
-                //     .stream()
-                //     .filter(obj -> obj.hasWon())
-                //     .findFirst();
-
-                // if (winningBoard.isPresent()) {
-                //     GiantSquid.WINNER = winningBoard.get();
-                //     GiantSquid.lastNumber = Integer.parseInt(nextDraw);
-                //     break;
-                // }
-            // }
-
-            // GiantSquid.WINNER.printBoard();
-            // System.out.println(GiantSquid.WINNER.getFinalScore(GiantSquid.lastNumber));
-
         } catch (IOException e) {}
-
-
-
     }
 }
