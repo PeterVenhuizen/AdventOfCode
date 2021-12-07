329be0c87ec96dae19b2e64216a174197ea6bec7
PeterVenhuizen <peter_venhuizen@hotmail.com>
2021-12-07 08:20:20 +0100

    Refactor to use BinaryOperator for the fuel calculation, making method for part 1 and 2 the same

--- theTreacheryOfWales/theTreacheryOfWales.java
+++ theTreacheryOfWales/theTreacheryOfWales.java
@@ -1,10 +1,11 @@
 import java.io.*;
 import java.util.*;
+import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 
 public class theTreacheryOfWales {
     
-    public static List<Integer> getInput(String inputFile) throws IOException {
+    public static List<Integer> getCrabPositions(String inputFile) throws IOException {
         Scanner sc = new Scanner(new File(inputFile));
 
         List<Integer> horizontalPos = Arrays.asList(sc.nextLine().split(","))
@@ -26,39 +27,19 @@ public class theTreacheryOfWales {
         }  
     }
 
-    public static int getMinFuel(List<Integer> positions) {
+    public static int getLowestFuelRequirement(List<Integer> positions, BinaryOperator<Integer> func) {
+
         Integer minFuelReq = Integer.MAX_VALUE;
 
         int low = Collections.min(positions);
         int high = Collections.max(positions);
 
-        System.out.println(low + " : " + high);
         for (int pos = low; pos <= high; pos++) {
             int thisPos = pos;
             int fuel = positions.stream()
-                .map(p -> Math.abs(p - thisPos))
+                .map(p -> func.apply(p, thisPos))
                 .reduce(0, Integer::sum);
-            // System.out.println(pos + ": " + fuel);
 
-            if (fuel < minFuelReq) 
-                minFuelReq = fuel;
-        }
-
-        return minFuelReq;
-    }
-
-    public static int getExpensiveFuel(List<Integer> positions) {
-        Integer minFuelReq = Integer.MAX_VALUE;
-
-        int low = Collections.min(positions);
-        int high = Collections.max(positions);
-
-        System.out.println(low + " : " + high);
-        for (int pos = low; pos <= high; pos++) {
-            int thisPos = pos;
-            int fuel = positions.stream()
-                .map(p -> getCrabFuel(Math.abs(p - thisPos)))
-                .reduce(0, Integer::sum);
             if (fuel < minFuelReq) 
                 minFuelReq = fuel;
         }
@@ -68,18 +49,18 @@ public class theTreacheryOfWales {
     
     public static void main(String[] args) {
         try {
-            // List<Integer> positions = getInput("test_input.txt");
-            List<Integer> positions = getInput("input.txt");
-            // System.out.println(positions);
-            // System.out.println(getMinFuel(positions));
-            System.out.println(getExpensiveFuel(positions));
 
-            // System.out.println(getCrabFuel(1));
-            // System.out.println(getCrabFuel(2));
-            // System.out.println(getCrabFuel(3));
-            // System.out.println(getCrabFuel(4));
-            // System.out.println(getCrabFuel(5));
-            // System.out.println(getCrabFuel(11));
+            BinaryOperator<Integer> constantFuelRate = (x, y) -> Math.abs(x - y);
+            BinaryOperator<Integer> increasingFuelRate = (x, y) -> getCrabFuel(Math.abs(x - y));
+
+            List<Integer> testPositions = getCrabPositions("test_input.txt");
+            List<Integer> positions = getCrabPositions("input.txt");
+
+            System.out.println("Test part 1 (should be 37): " + getLowestFuelRequirement(testPositions, constantFuelRate));
+            System.out.println("Answer part 1: " + getLowestFuelRequirement(positions, constantFuelRate));
+            
+            System.out.println("Test part 2 (should be 168): " + getLowestFuelRequirement(testPositions, increasingFuelRate));
+            System.out.println("Answer part 2: " + getLowestFuelRequirement(positions, increasingFuelRate));
 
         } catch (IOException e) {}
     }
