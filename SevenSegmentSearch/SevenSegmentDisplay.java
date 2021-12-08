14192783a32cf709e7fc2f9ddac4d4a8a93205a7
PeterVenhuizen <peter_venhuizen@hotmail.com>
2021-12-08 12:31:21 +0100

    More cleanup and added comments

--- SevenSegmentSearch/SevenSegmentDisplay.java
+++ SevenSegmentSearch/SevenSegmentDisplay.java
@@ -15,8 +15,6 @@ public class SevenSegmentDisplay {
         int count = 0;
         int numbers[] = { 2, 3, 4, 7 };
         for (String segment : this.fourDigitOutput) {
-            // String s = segment;
-            // System.out.println(segment + ": " + segment.length());
             if (IntStream.of(numbers).anyMatch(n -> n == segment.length())) {
                 count++;
             }
@@ -38,53 +36,24 @@ public class SevenSegmentDisplay {
             .collect(Collectors.toList());
     }
 
-    private List<String> diff(List<String> bigger, List<String> smaller) {
-        return bigger.stream()
-            .filter(e -> !smaller.contains(e))
-            .collect(Collectors.toList());
-    }
-
-    // private <T> boolean containsAll(List<T> list1, List<T> list2) {
-    //     for (T l: list1) {
-    //         if (!list2.contains(l)) 
-    //             return false;
-    //     }
-    //     return true;
-    // }
-
-    private boolean containsAll(String s1, String s2) {
-        List<String> list1 = stringToList(s1);
-        List<String> list2 = stringToList(s2);
-        for (String l: list1) {
-            if (!list2.contains(l)) 
-                return false;
-        }
-        return true;
+    private boolean containsAll(String subList, String superList) {
+        return stringToList(superList).containsAll(stringToList(subList));
     }
 
     private String deduceNumber(List<String> possibles, String filterOn, 
         BiPredicate<String, String> filter) {
 
-        // String result = "";
-        // for (String str : possibles) {
-        //     if (filter.test(filterOn, str)) {
-        //         result = str;
-        //     }
-        // }
-        // return result;
-        
-        List<String> result = possibles.stream()
+        return possibles.stream()
             .filter(v -> filter.test(filterOn, v))
-            .collect(Collectors.toList());
-        return result.get(0);
+            .collect(Collectors.toList())
+            .get(0);
     }
 
     private String selectNotInList(List<String> possibles, List<String> exclude) {
-        for (String p : possibles) {
-            if (!exclude.contains(p))
-                return p;
-        };
-        return "";
+        return possibles.stream()
+            .filter(p -> !exclude.contains(p))
+            .collect(Collectors.toList())
+            .get(0);
     }
 
     private <T> boolean listEqualsIgnoreOrder(List<T> list1, List<T> list2) {
@@ -93,9 +62,8 @@ public class SevenSegmentDisplay {
 
     private String decodeDigit(List<String> digit, List<String> decoded) {
         for (int i = 0; i < decoded.size(); i++) {
-            List<String> dec = stringToList(decoded.get(i));
-            
-            if (listEqualsIgnoreOrder(digit, dec)) {
+            List<String> decodedList = stringToList(decoded.get(i));
+            if (listEqualsIgnoreOrder(digit, decodedList)) {
                return Integer.toString(i);
             }
         }
@@ -106,136 +74,44 @@ public class SevenSegmentDisplay {
 
     public int decode() {
 
-        // String[] segments = new String[] { "", "", "", "", "", "", "" };
+        // segments I can't map to a number yet
+        List<String> segmentsOfLengthFive = this.segmentMap.get(5);
+        List<String> segmentsOfLengthSix = this.segmentMap.get(6);
 
+        // numbers I already know
         String one = this.segmentMap.get(2).get(0);
         String four = this.segmentMap.get(4).get(0);
         String seven = this.segmentMap.get(3).get(0);
         String eight = this.segmentMap.get(7).get(0);
 
-        List<String> segmentsOfLengthFive = this.segmentMap.get(5);
-        List<String> segmentsOfLengthSix = this.segmentMap.get(6);
-
-        // List<String> one = stringToList(this.segmentMap.get(2).get(0));
-        // List<String> four = stringToList(this.segmentMap.get(4).get(0));
-        // List<String> seven = stringToList(this.segmentMap.get(3).get(0));
-        // List<String> eight = stringToList(this.segmentMap.get(7).get(0));
-
-        // difference between one and seven goes to index 0
-        // List<String> seg0 = diff(seven, one);
-        // segments[0] = seg0.get(0);
-       
         // three is the only segment of length 5 that contains all values of seven
         String three = deduceNumber(segmentsOfLengthFive, seven, (x, y) -> containsAll(x, y));
-        // List<String> three = this.segmentMap.get(5)
-        //     .stream()
-        //     .filter(e -> containsAll(seven, stringToList(e)))
-        //     .collect(Collectors.toList());
-        // System.out.println("three: " + three);
-        // three = stringToList(three.get(0));
 
         // six is the only element of length 6 that does not contain all values of seven
         String six = deduceNumber(segmentsOfLengthSix, seven, (x, y) -> !containsAll(x, y));
-        // List<String> six = this.segmentMap.get(6)
-        //     .stream()
-        //     .filter(e -> !containsAll(seven, stringToList(e)))
-        //     .collect(Collectors.toList());
-        // System.out.println("six: " + six);
-        // six = stringToList(six.get(0));
 
         // all elements of five appear in six
         String five = deduceNumber(segmentsOfLengthFive, six, (x, y) -> containsAll(y, x));
-        // List<String> six2 = six;
-        // List<String> five = this.segmentMap.get(5)
-        //     .stream()
-        //     .filter(e -> containsAll(stringToList(e), six2))
-        //     .collect(Collectors.toList());
-        // System.out.println("five: " + five);
-        // five = stringToList(five.get(0));
 
         // nine contains all values of four
         String nine = deduceNumber(segmentsOfLengthSix, four, (x, y) -> containsAll(x, y));
-        // List<String> nine = this.segmentMap.get(6)
-        //     .stream()
-        //     .filter(e -> containsAll(four, stringToList(e)))
-        //     .collect(Collectors.toList());
-        // System.out.println("nine: " + nine);
-        // nine = stringToList(nine.get(0));
-        // System.out.println(nine);
-        // System.out.println(eight);
 
-        // difference of nine and eight goes to index 4
-        // List<String> seg4 = diff(eight, nine);
-        // segments[4] = seg4.get(0);
-        // System.out.println(seg4);
-
-        // two is the only number of length 5 that does contain segment 4
+        // two is the only remaining number of length 5 that I don't yet know
         String two = selectNotInList(segmentsOfLengthFive, Arrays.asList(three, five));
-        // List<String> two = this.segmentMap.get(5)
-        //     .stream()
-        //     .filter(e -> diff(seg4, stringToList(e)).size() == 0)
-        //     .collect(Collectors.toList());
-        // two = stringToList(two.get(0));
-        // System.out.println(two);
 
-        // difference of one and two goes to index 5
-        // List<String> seg5 = diff(one, two);
-        // segments[5] = seg5.get(0);
-
-        // difference of seg5 and one goes to index 2
-        // List<String> seg2 = diff(one, seg5);
-        // segments[2] = seg2.get(0);
-
-        // for (String l6: this.segmentMap.get(6)) {
-        //     System.out.println(l6);
-        //     System.out.println(containsAll(seven, stringToList(l6)));
-        // }
+        // zero is the only remaining number of length 6 that I don't yet know
         String zero = selectNotInList(segmentsOfLengthSix, Arrays.asList(six, nine));
 
-
-        // zero is the only remaining number of length 6
-        // System.out.println(this.segmentMap.get(6));
-        // List<String> zero = new ArrayList<>();
-        // for (String l6: this.segmentMap.get(6)) {
-        //     if (!containsAll(stringToList(l6), six) && !containsAll(stringToList(l6), nine))
-        //         zero = stringToList(l6);
-        // }
-        // System.out.println(zero);
-
-        // difference between 8 and 0 goes to index 3
-        // List<String> seg3 = diff(eight, zero);
-        // segments[3] = seg3.get(0);
-
-        // for (String l6: this.segmentMap.get(6)) {
-        //     System.out.println(l6 + ": " + diff(seg2, stringToList(l6)));
-        // }
-
-        // difference of seg2+3+5 with four is seg1
-        // List<String> seg1 = diff(four, Arrays.asList(seg2.get(0), seg3.get(0), seg5.get(0)));
-        // segments[1] = seg1.get(0);
-
-        // segment 6 is the difference of the segments list and eight
-        // List<String> seg6 = diff(eight, Arrays.asList(segments));
-        // segments[6] = seg6.get(0);
-
-        // final
-        // System.out.println(Arrays.toString(segments));
-
-        // List<String> five = Arrays.asList(segments[0], segments[1], segments[3], segments[5], segments[6]);
-
-        // List<List<String>> decoded = Arrays.asList(zero, one, two, three, four, five, six, seven, eight, nine);
+        // all decoded numbers
         List<String> decoded = Arrays.asList(zero, one, two, three, four, five, six, seven, eight, nine);
 
-
-
+        // decode the output
         String output = "";
         for (String digit: this.fourDigitOutput) {
-            // System.out.println(digit);
             output += decodeDigit(stringToList(digit), decoded);
         }
-        System.out.println(output);
+        // System.out.println(output);
 
-        // return 0;
         return Integer.parseInt(output);
     }
 
--- SevenSegmentSearch/SevenSegmentSearch.java
+++ SevenSegmentSearch/SevenSegmentSearch.java
@@ -1,5 +1,3 @@
-import java.nio.file.FileAlreadyExistsException;
-
 import java.io.*;
 import java.util.*;
 
@@ -11,13 +9,9 @@ public class SevenSegmentSearch {
         Scanner sc = new Scanner(new File(inputFile));
         while (sc.hasNext()) {
             String[] line = sc.nextLine().split(" ");
-            // System.out.println(Arrays.asList(line).indexOf("|"));
             int pipeIndex = Arrays.asList(line).indexOf("|");
             String[] uniqueSignalPattern = Arrays.copyOfRange(line, 0, pipeIndex);
             String[] fourDigitOutput = Arrays.copyOfRange(line, pipeIndex + 1, line.length);
-
-            // System.out.println(Arrays.toString(uniqueSignalPattern));
-            // System.out.println(Arrays.toString(fourDigitOutput));
 
             displays.add(new SevenSegmentDisplay(uniqueSignalPattern, fourDigitOutput));
         }
@@ -26,32 +20,41 @@ public class SevenSegmentSearch {
         return new DisplayGroup(displays);
     }
     public static void main(String[] args) {
+        /* 
+            Segment configuration: 
+                aaaa
+               b    c
+               b    c
+                dddd
+               e    f
+               e    f
+                gggg
 
-        // Map<Integer, Map<Integer, String> segments = new HashMap<HashMap<>>();
-
-        // 0: abc-efg  - 6
-        // 1: --c--f-  - 2
-        // 2: a-cde-g  - 5
-        // 3: a-cd-fg  - 5
-        // 4: -bcd-f-  - 4
-        // 5: abc--fg  - 5
-        // 6: ab-defg  - 6
-        // 7: a-c--f-  - 3
-        // 8: abcdefg  - 7
-        // 9: abcd-fg  - 6
-
-        // 0: abc-efg  - 6
-        // 6: ab-defg  - 6
-        // 7: a-c--f-  - 3
-        // 9: abcd-fg  - 6
+            
+            Digit: composing segments - number of segments in digit
+            0: abc-efg  - 6
+            1: --c--f-  - 2
+            2: a-cde-g  - 5
+            3: a-cd-fg  - 5
+            4: -bcd-f-  - 4
+            5: abc--fg  - 5
+            6: ab-defg  - 6
+            7: a-c--f-  - 3
+            8: abcdefg  - 7
+            9: abcd-fg  - 6
+        */
 
         try {
             DisplayGroup testDisplay = readyDisplay("test_input.txt");
             System.out.println("Test part 1 (should be 26): " + testDisplay.getOccurencesOf1478());
-            System.out.println(testDisplay.decodeEverything());
 
-            // DisplayGroup display = readyDisplay("input.txt");
-            // System.out.println("Answer part 1: " + display.getOccurencesOf1478());
+            DisplayGroup display = readyDisplay("input.txt");
+            System.out.println("Answer part 1: " + display.getOccurencesOf1478());
+                        
+            System.out.println("Test part 2 (should be 61229): " + testDisplay.decodeEverything());
+            System.out.println("Answer part 2: " + display.decodeEverything());
+
+
         
         } catch (FileNotFoundException e) {}
 
