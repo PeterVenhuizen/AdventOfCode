3270411c0e2026f6de551b540e151d5be4578128
PeterVenhuizen <peter_venhuizen@hotmail.com>
2021-12-10 14:29:29 +0100

    Answer part 2 also there

--- 2015/Day1-NotQuiteLisp/not_quite_lisp.rb
+++ 2015/Day1-NotQuiteLisp/not_quite_lisp.rb
@@ -1,10 +1,8 @@
 #!/usr/bin/env ruby
 
 class Instructions
-  attr_accessor :floor
 
   def initialize(input_file)
-    @floor = 0
     @instructions = read_file(input_file)[0]
   end
 
@@ -13,8 +11,21 @@ class Instructions
   end
 
   def follow_instructions
-    @instructions.split('').each { |c| @floor += c == '(' ? 1 : -1 }
-    @floor
+    floor = 0
+    @instructions.each_char { |c| 
+      floor += c == '(' ? 1 : -1 
+    }
+    return floor
+  end
+
+  def when_do_I_enter_the_basement
+    floor = 0
+    @instructions.each_char.with_index(1) { |char, i|
+      floor += char == '(' ? 1 : -1
+      if floor == -1
+        return i
+      end
+    }
   end
 end
 
@@ -22,6 +33,8 @@ if __FILE__ == $0
   test = Instructions.new "test_input.txt"
   puts test.follow_instructions
 
-  part1 = Instructions.new "input.txt"
-  puts "Answer part 1: #{part1.follow_instructions}"
+  answer = Instructions.new "input.txt"
+  puts "Answer part 1: #{answer.follow_instructions}"
+
+  puts "Answer part 2: #{answer.when_do_I_enter_the_basement}"
 end
