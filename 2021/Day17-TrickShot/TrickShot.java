2a6731c1301b3b51ace08f36a2c93ea646667360
PeterVenhuizen <peter_venhuizen@hotmail.com>
2021-12-17 15:34:30 +0100

    Made code more readable

--- 2021/Day17-TrickShot/TrickShot.java
+++ 2021/Day17-TrickShot/TrickShot.java
@@ -1,79 +1,58 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class TrickShot {
-
-	/* 
-		Each step:
-			x + x + (x > 0 ? -1 : 1)
-			y + y - 1
-
-
-		TEST: target area: x=20..30, y=-10..-5	
-		ANSWER: target area: x=79..137, y=-176..-117
-	*/
-
-
 
 	public static void bruteForce() {
 
-		// int MIN_X = 20, MAX_X = 30;
-		// int MIN_Y = -10, MAX_Y= -5;
-		int MIN_X = 79, MAX_X = 137;
-		int MIN_Y = -176, MAX_Y= -117;
+		int MIN_X = 20, MAX_X = 30;
+		int MIN_Y = -10, MAX_Y= -5;
+		// int MIN_X = 79, MAX_X = 137;
+		// int MIN_Y = -176, MAX_Y= -117;
+
+		List<Integer> maxYForTrajectoriesCrossingTargetArea = new ArrayList<>();
+		List<String> startingVelocitiesHittingTargetArea = new ArrayList<>();
 
 		for (int x = 0; x < 500; x++) {
 			for (int y = -500; y < 500; y++) {
 
-				// int x = 8;
-				// int y = 0;
+				// init variables for this round
+				int localX = x, localY = y;
+				int dX = x, dY = y;
+				int maxForThisTrajectory = Integer.MIN_VALUE;
 
-				int X = x;
-				int Y = y;
-				int MAX = Integer.MIN_VALUE;
+				// run while hasn't passed the target area yet
+				while (localX <= MAX_X || localY >= MIN_Y) {
 
-				// if (X >= MIN_X && X <= MAX_X && Y >= MIN_Y && Y <= MAX_Y) {
-				// 	System.out.println("x: " + x + "," + y + " MAX Y: " + MAX);
-				// 	// System.out.println("x: " + x + "," + y);
-				// }
+					if (localY > maxForThisTrajectory)
+						maxForThisTrajectory = localY;
 
-
-				int DELTA_Y = Y;
-				int DELTA_X = X;
-				while (X <= MAX_X || Y >= MIN_Y) {
-				// while (X <= MAX_X) {
-
-					if (X >= MIN_X && X <= MAX_X && Y >= MIN_Y && Y <= MAX_Y) {
-						System.out.println("x: " + x + "," + y + " MAX Y: " + MAX);
-						// System.out.println("x: " + x + "," + y);
+					boolean reachedTargetArea = localX >= MIN_X && localX <= MAX_X
+						&& localY >= MIN_Y && localY <= MAX_Y;
+					if (reachedTargetArea) {
+						maxYForTrajectoriesCrossingTargetArea.add(maxForThisTrajectory);
+						startingVelocitiesHittingTargetArea.add(x + "," + y);
 						break;
 					}
 
-					if (DELTA_X != 0) {
-						--DELTA_X;
-					}
-					X += DELTA_X;
+					// dX changes until dX is zero
+					if (dX != 0) --dX;
+					localX += dX;
 
-					// int DELTA_X = 0;
-					// if (X != 0) {
-					// 	DELTA_X = (X > 0) ? --X : ++X;
-					// }
-					// X += DELTA_X;					
-					
-					--DELTA_Y;
-					Y += DELTA_Y;
+					// dY get's decremented each step
+					localY += --dY;
 
-					// System.out.println(X + "," + Y + " " + DELTA_Y);
-
-					if (Y > MAX) MAX = Y;
-
-					if (DELTA_Y < MIN_Y) break;
-					// if (Y > MAX_Y) break;
-
-					// if (Y > MAX_Y)
-					// 	break;
+					// also stop if the trajectory would never reach the 
+					// target area
+					if (dY < MIN_Y) break;
 
 				}
 
 			}
 		}
+
+		System.out.println("Max y: " + Collections.max(maxYForTrajectoriesCrossingTargetArea));
+		System.out.println("Distinct initial velocities: " + startingVelocitiesHittingTargetArea.size());
 
 	}
 
